dc0dbefe33deac7bb353fe74b84d7c357c969f4c
LICHO <lichobaron@gmail.com>
2018-11-10 18:26:41 -0500

    editar reserva

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,10 +55,24 @@ class User extends Authenticatable
         return false;
     }
 
-    function hasReservations(){
-        if ($this->reservations()->where('state','ACTIVE')->count()>0){
+    function hasReservations($type){
+        $classroom = 0;
+        $instrument = 0;
+        foreach ($this->reservations as $reservation){
+            if($reservation->resource->type =='CLASSROOM'){
+                $classroom = $classroom+1;
+            }
+            if($reservation->resource->type =='INSTRUMENT'){
+                $instrument = $instrument+1;
+            }
+        }
+        if($type === 'CLASSROOM' && $classroom===1){
             return true;
         }
+        else if ($type === 'INSTRUMENT' && $instrument ===1){
+            return true;
+        }
+        return false;
     }
 
     function file(){
