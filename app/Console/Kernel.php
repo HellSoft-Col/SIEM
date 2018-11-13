d966041366e3649012e2a878da9d400ef2879339
Santiago Chaustre <santiago.chaustreperdomo@gmail.com>
2018-11-13 15:18:50 -0500

    Hand Over Ok

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,9 +2,11 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ValidateIncomingReservations;
 use App\Console\Commands\ValidateReservationsInTime;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -27,7 +29,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command(ValidateReservationsInTime::class)->hourly();
+        $schedule->command(ValidateReservationsInTime::class)->hourlyAt(17);
+        $schedule->command(ValidateIncomingReservations::class)->hourlyAt(17);
     }
 
     /**
