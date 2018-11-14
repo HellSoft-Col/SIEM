8e33079e0a95ad3d7e1578315835d9c91a97f3b8
Santiago Chaustre <santiago.chaustreperdomo@gmail.com>
2018-11-14 00:33:13 -0500

    Solving Table

--- resources/views/GeneralViews/ResourcesViews/Availability/view_instrument.blade.php
+++ resources/views/GeneralViews/ResourcesViews/Availability/view_instrument.blade.php
@@ -4,7 +4,7 @@
           href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css"/>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ asset('/css/reserve/content.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/resourceStyle/content.css') }}">
 @endsection
 
 @section('content')
